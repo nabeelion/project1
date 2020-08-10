d2643143d0568f3cfb0873e4aa4f02ceab382429
yousuf <yousuf.drive@gmail.com>
2020-08-10 14:13:09 +0530

    for the new branch.

--- project1/src/project1/Main.java
+++ project1/src/project1/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println(" Hi..!!");
 		System.out.println(" Hi Again. For Second Commit. ");
-		System.out.println();
+		System.out.println("In the new branch ");
 	}
 
 }
